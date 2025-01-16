65616db8c6e9ab3759fa1e8e6604724aca9a9683
Ponlponl123 <pattarapong.sowcharoen@gmail.com>
2025-01-16 21:17:55 +0700

    wrong version

--- src/app/app/updates/page.tsx
+++ src/app/app/updates/page.tsx
@@ -9,7 +9,7 @@ function Page() {
         <main id="app-panel">
             <main id="app-workspace">
                 <h1 className='text-2xl mb-4'>{language.data.app.updates.name}</h1>
-                <h2 className='text-5xl mt-6 flex gap-4 items-center'><Wrench size={48} weight='fill' /> {language.data.app.updates.latest}: 0.1.6-Release</h2>
+                <h2 className='text-5xl mt-6 flex gap-4 items-center'><Wrench size={48} weight='fill' /> {language.data.app.updates.latest}: Pre-Release 0.1.6</h2>
             </main>
         </main>
     )
