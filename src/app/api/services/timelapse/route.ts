27cda8262f64945097ef4202bba4b2bbfa5a98dd
Ponlponl123 <pattarapong.sowcharoen@gmail.com>
2025-01-19 12:00:20 +0700

    remove sensitive code (server directly connection to database)

--- src/app/api/services/timelapse/route.ts
+++ src/app/api/services/timelapse/route.ts
@@ -1,54 +1,44 @@
-import mysql2, { RowDataPacket } from "mysql2/promise";
-import query from '@/server-side-api/database/query';
+import axios, { HttpStatusCode } from "axios";
 
-interface dataset {
-  time: string;
-  shards: {
-    id: number;
-    ping: string;
-  }[]
+export type ShardData = {
+  [key: string]: number;
 }
 
-export interface DatasetRow extends RowDataPacket, dataset {}
+export type dataset = {
+  time: string;
+  shards: ShardData;
+}
 
 export async function GET() {
-  const result = await query(`
-    WITH numbered_rows AS (
-      SELECT time, pingtomaster AS ping, shardid,
-        UNIX_TIMESTAMP(time) DIV 300 AS time_slot,
-        ROW_NUMBER() OVER (PARTITION BY UNIX_TIMESTAMP(time) DIV 300, shardid ORDER BY time DESC) AS rn
-      FROM pona_heartbeat_interval
-      WHERE time >= NOW() - INTERVAL 24 HOUR
-    )
-    SELECT FROM_UNIXTIME(time_slot * 300) AS time,
-      CONCAT('{', GROUP_CONCAT(CONCAT('"', shardid, '": ', ping)), '}') AS shards
-    FROM numbered_rows
-    WHERE rn = 1
-    GROUP BY time_slot
-    ORDER BY time_slot DESC;
-  `);
+  const result = await axios.get('https://api.ponlponl123.com/v1/services/pona/response');
   if (result) {
-    const results: [mysql2.QueryResult, DatasetRow[]] = JSON.parse(result);
-    const dataSet: dataset[] = (results[0] as dataset[]).map(field => {
-      return {
-        time: new Date(field.time).toLocaleString([], {
-          day: 'numeric', 
-          month: 'short', 
-          year: 'numeric', 
-          hour: 'numeric', 
-          minute: 'numeric'
-        }),
-        shards: JSON.parse(String(field.shards))
-      }
-    })
+    const results = result.data;
+    if ( results.data ) {
+      const dataSet: dataset[] = (results.data as dataset[]).map(field => {
+        return {
+          time: new Date(field.time).toLocaleString([], {
+            day: 'numeric', 
+            month: 'short', 
+            year: 'numeric', 
+            hour: 'numeric', 
+            minute: 'numeric'
+          }),
+          shards: field.shards
+        }
+      })
+      return Response.json({
+        message: 'OK',
+        timelapse: dataSet,
+      }, {status: HttpStatusCode.Ok, headers: {'Content-Type': 'application/json'}});
+    }
     return Response.json({
-      message: 'OK',
-      timelapse: dataSet,
-    }, {status: 200});
+      message: 'API Endpoint Error',
+      timestamp: new Date().toISOString()
+    }, {status: HttpStatusCode.ServiceUnavailable});
   } else {
     return Response.json({
       message: 'Internal Server Error',
       timestamp: new Date().toISOString(),
-    }, {status: 500});
+    }, {status: HttpStatusCode.InternalServerError});
   }
 }
