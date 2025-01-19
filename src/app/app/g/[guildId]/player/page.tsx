4b099bc8f032e14cd1ce84d35a3fe46d92b8332a
Ponlponl123 <pattarapong.sowcharoen@gmail.com>
2025-01-19 21:01:10 +0700

    player will avaliable soon!

--- src/app/app/g/[guildId]/player/page.tsx
+++ src/app/app/g/[guildId]/player/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { Spinner } from '@nextui-org/react';
 import { useLanguageContext } from '@/contexts/languageContext';
 import { useDiscordGuildInfo } from '@/contexts/discordGuildInfo';
-import { Guitar, Wrench } from '@phosphor-icons/react/dist/ssr';
+import { GitPullRequest, Guitar } from '@phosphor-icons/react/dist/ssr';
 
 function Page() {
   const { guild } = useDiscordGuildInfo();
@@ -18,7 +18,7 @@ function Page() {
               <h1 className='text-base'>{guild.name}</h1>
               <h1 className='text-5xl mt-4 flex items-center gap-4'><Guitar weight='fill' size={48} /> {language.data.app.guilds.player.name}</h1>
               <div className='w-full max-w-screen-lg mt-16 gap-4 flex flex-col items-center justify-center text-center' style={{minHeight: '48vh'}}>
-                <Wrench size={64} weight='bold' />
+                <GitPullRequest size={64} weight='bold' />
                 <h1 className='text-4xl max-w-xl'>{language.data.app.guilds.player.dev}</h1>
               </div>
             </>
