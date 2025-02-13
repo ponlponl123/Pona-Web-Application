f3719bc4723b25b2b8416073c93ce8f714f539e6
Ponlponl123 <pattarapong.sowcharoen@gmail.com>
2025-02-13 22:56:20 +0700

    connect socket to external endpoint

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  crossOrigin: 'anonymous',
   images: {
     remotePatterns: [
       {
--- src/app/app/g/[guildId]/player/socket.ts
+++ src/app/app/g/[guildId]/player/socket.ts
@@ -2,6 +2,17 @@
 
 import { Manager } from "socket.io-client";
 
-export const ws_manager = new Manager(`/socket.io/`, {
+const getEndpoint = () => {
+  if (typeof window !== 'undefined') {
+    return process.env.NEXT_PUBLIC_PONA_APPLICATION_WS_ENDPOINT || window.location.origin;
+  }
+  return process.env.NEXT_PUBLIC_PONA_APPLICATION_WS_ENDPOINT || '';
+};
+
+const endpoint_port = process.env.NEXT_PUBLIC_PONA_APPLICATION_WS_ENDPOINT_PORT || '';
+
+const endpoint = getEndpoint();
+
+export const ws_manager = new Manager(`${endpoint}${endpoint_port?':'+endpoint_port:''}/socket.io/`, {
   reconnectionDelayMax: 10000,
 });
